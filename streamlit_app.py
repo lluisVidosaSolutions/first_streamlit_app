c1e95c02c33e12d3ea63dd5cf422876f2e6ce01e
lluisVidosaSolutions <39332863+lluisVidosaSolutions@users.noreply.github.com>
2023-03-13 14:56:01 +0000

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title('My Parents new Healthy dinner')
+
+st.header('Breakfast menu')
+
+st.text('Omega 3')
+st.text('Kale, spinach and smoothy')
+st.text('Hard boiled free range')
+
+
