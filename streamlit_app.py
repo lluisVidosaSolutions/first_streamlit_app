224327faf2e4bd5104905a7124593a3b0d43cd61
lluisVidosaSolutions <39332863+lluisVidosaSolutions@users.noreply.github.com>
2023-03-14 10:16:34 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 st.title('My Parents new Healthy dinner')
 
-streamlit.header('Breakfast Favourites')
-streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avocado Toast')
+st.header('Breakfast Favourites')
+st.text('🥣Omega 3 & Blueberry Oatmeal')
+st.text('🥗Kale, Spinach & Rocket Smoothie')
+st.text('🐔Hard-Boiled Free-Range Egg')
+st.text('🥑🍞Avocado Toast')
 
 
